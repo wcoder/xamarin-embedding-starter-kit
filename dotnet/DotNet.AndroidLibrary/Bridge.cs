0ed19d55c7aa6bff1f561d192a97c3d7563157fb
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-05-23 15:19:23 +0300

    [dotnet] Add sample class

---
+++ dotnet/DotNet.AndroidLibrary/Bridge.cs
@@ -0,0 +1,16 @@
+﻿//
+// Bridge.cs
+// Copyright (c) 2018 Yauheni Pakala
+//
+
+namespace DotNet.AndroidLibrary
+{
+    public class Bridge
+    {
+        public string TestStr
+        {
+            get;
+            set;
+        }
+    }
+}
