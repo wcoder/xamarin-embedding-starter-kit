3737011389a40180f7018947fcba00a27f1c90db
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-05-26 19:57:04 +0300

    [dotnet] add ios library

---
+++ dotnet/DotNet.IosLibrary/Bridge.cs
@@ -0,0 +1,16 @@
+﻿//
+// MyClass.cs
+// Copyright (c) 2018 Yauheni Pakala
+//
+
+namespace DotNet.IosLibrary
+{
+    public class Bridge
+    {
+        public string TestStr
+        {
+            get;
+            set;
+        }
+    }
+}
