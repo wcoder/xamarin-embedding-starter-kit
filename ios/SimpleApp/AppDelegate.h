e1343ac56718307acc458ac4d43dd5442e63f410
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-05-27 12:15:25 +0300

    [ios] add empty ios project

---
+++ ios/SimpleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleApp
+//
+//  Created by Yauheni Pakala on 5/27/18.
+//  Copyright © 2018 Yauheni Pakala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/SimpleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleApp
+//
+//  Created by Yauheni Pakala on 5/27/18.
+//  Copyright © 2018 Yauheni Pakala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
