03a7425bedbfe8a4986fbb36d3de45206b2b40b5
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-05-28 21:13:36 +0300

    [android] update build-script

--- build-android.sh
+++ build-android.sh
@@ -2,6 +2,7 @@
 
 PROJECT_NAME=DotNet.AndroidLibrary
 OUTPUT_PATH=output-android
+ANDROID_PROJECT_PATH=../android
 CONFIG=Release
 
 cd dotnet
@@ -24,3 +25,11 @@ mono ../e4k/build/lib/$CONFIG/Embeddinator-4000.exe \
     --compile \
     --verbose \
     $PROJECT_NAME/bin/$CONFIG/$PROJECT_NAME.dll
+
+# Copy AAR to Android project
+cp $OUTPUT_PATH/$PROJECT_NAME.aar $ANDROID_PROJECT_PATH/$PROJECT_NAME/$PROJECT_NAME.aar
+echo $ANDROID_PROJECT_PATH/$PROJECT_NAME/$PROJECT_NAME.aar
+
+# Build Android project
+#cd $ANDROID_PROJECT_PATH
+#./gradlew assembleDebug
