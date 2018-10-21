b3dab5fc889b92ba6db01e8fcef81bcc9924902e
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-10-21 21:46:07 +0300

    [android] update project

--- build-android.sh
+++ build-android.sh
@@ -18,7 +18,7 @@ rm -Rf $PROJECT_NAME/bin/$CONFIG/
 msbuild $PROJECT_NAME/$PROJECT_NAME.csproj /p:Configuration=$CONFIG
 
 # Run E4K
-mono ../e4k/build/lib/$CONFIG/Embeddinator-4000.exe \
+mono ../e4k/build/lib/Release/Embeddinator-4000.exe \
     --gen=Java \
     --platform=Android \
     --outdir=$OUTPUT_PATH \
