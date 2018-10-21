38998149a79d8fec8e89919a1165c9f95966af2c
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-10-21 22:09:46 +0300

    [android] fix sample

--- android/app/src/main/java/name/pakalo/evgeniy/simpleapp/MainActivity.java
+++ android/app/src/main/java/name/pakalo/evgeniy/simpleapp/MainActivity.java
@@ -2,6 +2,10 @@ package name.pakalo.evgeniy.simpleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+// 1 - import .NET library
+import dotnet_androidlibrary.dotnet.androidlibrary.Bridge;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv_hello);
+
+        // 2 - create instance of .NET class
+        Bridge bridge = new Bridge();
+
+        // 3 - call method with parameter
+        bridge.setTestStr("Test str from .NET");
+
+        // 4 - get result
+        String str = bridge.getTestStr();
+
+        tv.setText(str);
     }
 }
